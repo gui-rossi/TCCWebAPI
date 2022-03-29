5fc02301abe19f12457417fb53d7a2d4bea4f243
guirossi <guizo.rossi@gmail.com>
2022-03-29 20:01:10 -0300

    remove ignored files

--- TCCRepositories/Interfaces/IUserRepository.cs
+++
@@ -1,9 +0,0 @@
-﻿using TCCDomain.Entities;
-
-namespace TCCRepositories.Interfaces
-{
-    public interface IUserRepository : IGenericRepository<UserEntity>
-    {
-        Task<UserEntity> FindByEmailAsync(string email);
-    }
-}
