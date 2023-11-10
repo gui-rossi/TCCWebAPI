837a50b88270f926d18202133403952483f7c7ec
gui-rossi <guizo.rossi@gmail.com>
2023-11-09 21:23:28 -0300

    added migrations

--- TCCRepositories/Migrations/20231109225037_remove_start_time_detection_end_time_detection_from_config_table.Designer.cs
+++ TCCRepositories/Migrations/20231109225037_remove_start_time_detection_end_time_detection_from_config_table.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using TCCRepositories.TCCContext;
 
@@ -11,9 +12,10 @@ using TCCRepositories.TCCContext;
 namespace TCCRepositories.Migrations
 {
     [DbContext(typeof(DatabaseContext))]
-    partial class DatabaseContextModelSnapshot : ModelSnapshot
+    [Migration("20231109225037_remove_start_time_detection_end_time_detection_from_config_table")]
+    partial class remove_start_time_detection_end_time_detection_from_config_table
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ TCCRepositories/Migrations/20231109225037_remove_start_time_detection_end_time_detection_from_config_table.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TCCRepositories.Migrations
+{
+    public partial class remove_start_time_detection_end_time_detection_from_config_table : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "StartTimeLight",
+                table: "Config",
+                newName: "StartTime");
+
+            migrationBuilder.RenameColumn(
+                name: "EndTimeLight",
+                table: "Config",
+                newName: "EndTime");
+
+            migrationBuilder.DropColumn(
+                name: "StartTimeDetection",
+                table: "Config");
+
+            migrationBuilder.DropColumn(
+                name: "EndTimeDetection",
+                table: "Config");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
